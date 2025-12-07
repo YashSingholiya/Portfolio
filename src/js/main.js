d7516e39abc358d85679aba1c6d54606ad60a11d
YashSingholiya <b23cs1081@iitj.ac.in>
2025-12-07 23:54:52 +0530

    Added logic for blur Header

--- src/js/main.js
+++ src/js/main.js
@@ -22,3 +22,11 @@ const linkAction = () =>{
     navMenu.classList.remove('show-menu')
 }
 navLink.forEach(n => n.addEventListener('click', linkAction))
+
+const blurHeader = () =>{
+    const header = document.getElementById('header')
+    // When the scroll is greater than 50 viewport height, add the blur-header class to the header tag
+    this.scrollY >= 50 ? header.classList.add('blur-header') 
+                       : header.classList.remove('blur-header')
+}
+window.addEventListener('scroll', blurHeader)
